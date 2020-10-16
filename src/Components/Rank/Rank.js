4da7e07885ad9a9ecda9cf9ceb0af946fb591eb2
Muhammad-Abdullah012 <ma9589094@gmail.com>
2020-10-16 15:33:54 +0500

    Changing something

--- src/Components/Rank/Rank.js
+++ src/Components/Rank/Rank.js
@@ -5,6 +5,7 @@ const Rank = ({user}) => {
 	return(
 			<div id = 'rank'>
 				<h3>Hi {user.firstName + " " + user.lastName}, Your current rank is {user.entries}.</h3>
+				<p>Paste link of any image in below, it will detect any faces in it... Give it a try!</p>
 			</div>
 		);
 }
